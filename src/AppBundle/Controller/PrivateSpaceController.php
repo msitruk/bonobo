cb465a05e0aeb0aeb789528101b3104b78c1e165
Maxime SITRUK <maxime.sitruk@gmail.com>
2017-06-27 23:01:14 +0200

    UPDATE : Version quasi finale, toutes les fonctionalités demandées sont là, il resterai quelques optimisation sur la génération des liens

--- src/AppBundle/Controller/PrivateSpaceController.php
+++ src/AppBundle/Controller/PrivateSpaceController.php
@@ -18,12 +18,6 @@ class PrivateSpaceController extends Controller
 
 	    $users = $repository->findAll();
 
-	    //M.S : small hack to remove admin from list because role field is serialized ...
-	    foreach ($users as $key => $value) {
-	    	if ($value->id == 1){
-	    		unset($users[$key]);
-	    	}
-	    }
 
 	    $userID = $this->getUser()->getId();
 
@@ -33,17 +27,29 @@ class PrivateSpaceController extends Controller
 	    $friends = $user->getMyFriends();
 	    dump($friends);
 
+	    //M.S : small hack to remove current user and admin from list because role field is serialized ...
+	    foreach ($users as $key => $value) {
+
+	    	//TODO M.S : Filtrer les users deja friends
+	    	// if (alreadyFriend($value, $friends)){
+	    	// if (in_array($value, $friends, true)){
+	    	// 	$users[$key]['alreadyFriend'] = true;
+	    	// }
+
+	    	if ($value->id == 1 || $value->id == $userID){
+	    		unset($users[$key]);
+	    	}
+	    }
+
+	    dump($users);
+
         return $this->render('AppBundle:PrivateSpace:private_space.html.twig', array(
 			'users' => $users,
 			'friends' => $friends,
         ));
 	}
 
+		private function alreadyFriend(User $currentUser, User $otherUser){
+
+		}
 }
-
-
-/*
-    {% for friend in friends %}
-        <li>{{ friend.username|e }}</li>
-    {% endfor %}
-*/
